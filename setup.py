c31aa1793c3f034ea03513b0598a060535595700
WangHelin1997 <49939413+WangHelin1997@users.noreply.github.com>
2020-05-20 10:17:17 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nnAudio_plus",
+    version="0.0.1",
+    author="Helin Wang",
+    author_email="wanghl15@pku.edu.cn",
+    description="PyTorch implemention of audio processing functions based on nnAudio.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/WangHelin1997/nnAudio_plus",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
